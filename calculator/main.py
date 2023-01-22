6b3e6c7971fa012079a3c392bd127273cc887649
Sachin Rathod <119095339+caped-baldys@users.noreply.github.com>
2023-01-22 10:51:58 +0530

    python script

--- calculator/main.py
+++ calculator/main.py
@@ -13,7 +13,7 @@ class MainApp(App):
 
         main_layout = BoxLayout(orientation = 'vertical')
 
-        self.solution = TextInput(background_color = "black", foreground_color = "white"
+        self.solution = TextInput(background_color = "black", foreground_color = "white",readonly = True
                         )
 
         main_layout.add_widget(self.solution)
